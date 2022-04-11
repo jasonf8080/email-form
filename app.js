790bb01450bf1bec0228ec6b19bbdc3b81abcc96
jasonf8080 <87998220+jasonf8080@users.noreply.github.com>
2022-04-11 19:28:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form');
 const message = document.querySelector('.thanks')
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    //e.preventDefault();
     message.classList.add('active');
     console.log('message sent');
 })
